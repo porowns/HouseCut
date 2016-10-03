14cccf8f889e1a3539426b0181953eceb167187d
Chris <ckm13d@my.fsu.edu>
2016-10-03 10:51:54 -0400

    App.yaml for Google Cloud

--- server/test/routes/accounts/register.js
+++ server/test/routes/accounts/register.js
@@ -3,9 +3,19 @@ var expect = chai.expect;
 var login = require('./../../../accounts/login.js');
 var User = require('../models/user');
 
+var url = 'localhost';
+var port = '8080';
+var fullUrl = host + ':' + port;
+
 describe('login', function() {
   it('should return success: false if no data is supplied', function() {
-    $.post('localhost:8080/login', { }, function(data, textStatus) {
+    $.post(fullUrl + '/login', { }, function(data, textStatus) {
+      expect(data.success).to.equal(false);
+    });
+  });
+
+  it('should return success: false if no password is supplied', function() {
+    $.post(fullUrl + '/login', { email: 'c@c', username: 'c' }, function(data, textStatus) {
       expect(data.success).to.equal(false);
     });
   });
