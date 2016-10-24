6a2778948ee40e1748671b089605d9c101ac5717
Logan Vega <loganv14@live.com>
2016-10-24 15:14:33 -0400

    Changed methods from private to public
    Changed so that the server and household_member_class can properly interact with the class. Also improved formatting of the code.

--- housecut_admin.java
+++ housecut_admin.java
@@ -8,14 +8,14 @@ public class household_admin extends household_member_class
 
 	public household_admin(household_member_class member)
 	{
-    //conversion constructor converts household_member_class to household_admin
+  		//conversion constructor converts household_member_class to household_admin
 		giveAdminPrivileges(member);
 		
 	}
 
-	private bool removeHouseholdMember(household_member_class member)
+	public bool removeHouseholdMember(household_member_class member)
 	{
-    //fail if no members would be left in household
+    	//fail if no members would be left in household
 		if (household_vector.size() <= 1)  // begin c++ code lol
 			return false;
 		else {
@@ -24,19 +24,19 @@ public class household_admin extends household_member_class
 		}
 	}
 
-	private bool giveAdminPrivileges(household_member_class member)
+	public bool giveAdminPrivileges(household_member_class member)
 	{
 		if (isAnAdmin(member) == false) {
 			member.role = admin;
 			return true;
 		}
-    return false;
-      //TODO create conversion constructor to convert household_member_class to household_admin
+   	 return false;
+     	 //TODO create conversion constructor to convert household_member_class to household_admin
 	}
 
-	private bool revokeAdminPrivileges(household_admin admin)
+	public bool revokeAdminPrivileges(household_admin admin)
 	{
-      new household_member_class(admin);
-      //TODO create conversion constructor to convert household_admin to household_member_class
+      		new household_member_class(admin);
+     		 //TODO create conversion constructor to convert household_admin to household_member_class
 	}	
 }
