bc6894085bf4769e8f44962c1dda51f76f3e948e
njohnson11 <naj14c@my.fsu.edu>
2016-10-26 15:32:21 -0400

    Add files via upload

---
+++ login_confirmation_activity.java
@@ -0,0 +1,16 @@
+package com.example.android.housecut;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+/**
+ * Created by nickjohnson on 10/24/16.
+ */
+
+public class login_confirmation_activity extends AppCompatActivity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_confirmation);
+    }
+}
