9db44ae23b6c44c2d5f5344ad55b3354dc3de054
Simon Aizpurua <saa13b@my.fsu.edu>
2016-10-27 14:07:03 -0400

    Update
    Made bool functions less error prone and made them return a value.

--- housecut_admin.java
+++ housecut_admin.java
@@ -9,19 +9,19 @@ public class household_admin extends household_member_class
 	public household_admin(household_member_class member)
 	{
   		//conversion constructor converts household_member_class to household_admin
-		giveAdminPrivileges(member);
-		
+		giveAdminPrivileges(member);	
 	}
 
 	public bool removeHouseholdMember(household_member_class member)
 	{
     	//fail if no members would be left in household
-		if (household_vector.size() <= 1)  // begin c++ code lol
+		if (household_vector.size() <= 1)
 			return false;
 		else {
 			household.removeMember(member);
 			return true;
 		}
+		return false; 
 	}
 
 	public bool giveAdminPrivileges(household_member_class member)
@@ -30,13 +30,17 @@ public class household_admin extends household_member_class
 			member.role = admin;
 			return true;
 		}
-   	 return false;
+   	 	return false;
      	 //TODO create conversion constructor to convert household_member_class to household_admin
 	}
 
 	public bool revokeAdminPrivileges(household_admin admin)
 	{
-      		new household_member_class(admin);
-     		 //TODO create conversion constructor to convert household_admin to household_member_class
+      		if(isAnAdmin(admin) == true) {
+		//TODO create conversion constructor to convert household_admin to household_member_class
+		     new household_member_class(admin);
+		     return true;
+		 }
+		 return false;
 	}	
 }
