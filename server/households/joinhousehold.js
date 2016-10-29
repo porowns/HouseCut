4c674f2c108b06481a8de7f2d0e40d4f85196289
Porowns <porowns@gmail.com>
2016-10-29 15:26:55 -0400

    /joinhousehold, finds if user has a household or not

--- server/households/joinhousehold.js
+++ server/households/joinhousehold.js
@@ -1,5 +1,6 @@
 var Household = require('../models/household');
 var User = require('../models/user.js');
+var utilities = require('./../utilities.js');
 
 // To Depcryt Household Password
 var jwt = require('jsonwebtoken');
@@ -18,7 +19,25 @@ var decoded = jwtDecode(token);
 var currentUserId = decoded.id;
 
 // Access User
-User.findOne({ '_id' : currentUserId}, function(err, household) {
-
-}); // End of User.findOne  
+User.findOne({ '_id' : currentUserId}, function(err, user) {
+  if (err) {
+    throw err;
+  }
+  if (user) {
+    if (user.householdId != 0) {
+      res.json({
+        success: false,
+        message: "User already has a household"
+      });
+    }
+    else {
+      res.json({
+        success: true,
+        message: "User does not have a household"
+      });
+    }
+  }
+  else {
+  }
+}); // End of User.findOne
 } // End of Function
