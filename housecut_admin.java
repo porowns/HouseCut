6f70808d5f86c0f4cd6f579ae6afca7823953feb
Logan Vega <loganv14@live.com>
2016-10-31 14:04:22 -0400

    Remove Checking
    Checks removed from the class by request because they are done server-side.

--- housecut_admin.java
+++ housecut_admin.java
@@ -14,7 +14,7 @@ public class household_admin extends household_member_class
 
 	public bool removeHouseholdMember(household_member_class member)
 	{
-    	//fail if no members would be left in household
+    	//fail if no members would be left in household (done server-side?)
 		if (household_vector.size() <= 1)
 			return false;
 		else {
@@ -26,21 +26,16 @@ public class household_admin extends household_member_class
 
 	public bool giveAdminPrivileges(household_member_class member)
 	{
-		if (isAnAdmin(member) == false) {
 			member.role = admin;
+			household_admin admin = new household_admin(member);
 			return true;
-		}
-   	 	return false;
      	 //TODO create conversion constructor to convert household_member_class to household_admin
 	}
 
 	public bool revokeAdminPrivileges(household_admin admin)
 	{
-      		if(isAnAdmin(admin) == true) {
 		//TODO create conversion constructor to convert household_admin to household_member_class
-		     new household_member_class(admin);
-		     return true;
-		 }
-		 return false;
+		household_member_class member = new household_member_class(admin);
+		return true;
 	}	
 }
