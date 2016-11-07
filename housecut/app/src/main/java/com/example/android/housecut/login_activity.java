c8624e531e19f2de8d11473c7ee4690f8b18773d
njohnson11 <naj14c@my.fsu.edu>
2016-11-07 10:27:44 -0500

    Current
    Application current on Nick’s computer

--- housecut/app/src/main/java/com/example/android/housecut/login_activity.java
+++ housecut/app/src/main/java/com/example/android/housecut/login_activity.java
@@ -3,7 +3,6 @@ package com.example.android.housecut;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -31,19 +30,13 @@ public class login_activity extends AppCompatActivity {
 
 
         mEmailView = (EditText) findViewById(R.id.email1);
-
         mPasswordView = (EditText) findViewById(R.id.password1);
 
-        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
-                attemptLogin();
-                return true;
-            }
-        });
+
 
         Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
         Button mEmailRegisterButton = (Button) findViewById(R.id.email_register_button);
+
 
         mEmailSignInButton.setOnClickListener(new View.OnClickListener() {
             @Override
