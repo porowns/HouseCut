5f951d8b2282cc599429efa766caefe19ae294c0
Adam Faulkner <mcfaulky@gmail.com>
2016-11-10 15:55:51 -0500

    Added JSON handling, sent /setAdmin server request
    Worked with Logan to fix up housecut_admin.java

--- housecut_admin.java
+++ housecut_admin.java
@@ -1,7 +1,17 @@
 /* created by Logan Vega and Simon Aizpurua */
+/*Initial server & Json handling done by Adam Faulkner & Logan Vega*/
 
 import java.util.*;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import org.json.JSONObject;
+import org.json.JSONException;
 
 public class household_admin extends household_member_class
 {
@@ -9,7 +19,7 @@ public class household_admin extends household_member_class
 	public household_admin(household_member_class member)
 	{
   		//conversion constructor converts household_member_class to household_admin
-		giveAdminPrivileges(member);	
+		giveAdminPrivileges(member);
 	}
 
 	public bool removeHouseholdMember(household_member_class member)
@@ -22,14 +32,59 @@ public class household_admin extends household_member_class
 			household.removeMember(member);
 			return true;
 		}
-		return false; 
+		return false;
 	}
 
 	public bool giveAdminPrivileges(household_member_class member)
 	{
 		//only allowed if role of caller = admin
 		member.role = "admin";
-			
+
+		String url = request + "setadmin";
+		//Get /household /roommates
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+		conn.setDoOutput(true);
+		conn.setRequestMethod("POST");
+		conn.setRequestProperty("Content-Type", "application/json");
+		conn.setRequestProperty("Accept", "application/json");
+
+		JSONObject json = new JSONObject();
+		json.put("userId", member.getID());
+		json.put("setAdmin", true);
+		json.put("token", member.getToken());
+		String requestBody = json.toString();
+
+		OutputStreamWriter out = new OutputStreamWriter(conn.getOutputStream(), "UTF-8");
+
+		out.write(requestBody);
+		out.close();
+
+		//getResponseCode()!
+
+		BufferedReader in = new BufferedReader(
+												new InputStreamReader(
+														conn.getInputStream()));
+
+		StringBuffer result = new StringBuffer();
+		String line = "";
+		System.out.println("Output from Server .... \n");
+		while ((line = in.readLine()) != null) {
+			System.out.println(result);
+			result.append(line);
+		}
+
+			//JSON string returned by server
+		JSONObject data = new JSONObject(result);
+		Bool success = data.getBoolean("success");
+
+		if (success == true)
+			//setAdmin was successful!!
+		else {
+			//setAdmin failed
+			String message = data.getString("message");
+		}
+
 		return true;
 	}
 
@@ -37,7 +92,7 @@ public class household_admin extends household_member_class
 	{
 		//only allowed if role of caller = admin
 		admin.role = "member";
-		
+
 		return true;
-	}	
+	}
 }
