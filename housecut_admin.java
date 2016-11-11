aa9545a168b7bb0f13ac5baa59f985c9653f36fd
Logan Vega <loganv14@live.com>
2016-11-11 10:18:33 -0500

    Updated make admin and revoke admin functions
    updated both functions using household_member_class so that they can communicate with the server

--- housecut_admin.java
+++ housecut_admin.java
@@ -39,53 +39,65 @@ public class household_admin extends household_member_class
 	{
 		//only allowed if role of caller = admin
 		member.role = "admin";
+		try {
+			String url = request + "setadmin";
+			//Get /household /roommates
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
-		String url = request + "setadmin";
-		//Get /household /roommates
-		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setDoOutput(true);
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty("Content-Type", "application/json");
+			conn.setRequestProperty("Accept", "application/json");
 
-		conn.setDoOutput(true);
-		conn.setRequestMethod("POST");
-		conn.setRequestProperty("Content-Type", "application/json");
-		conn.setRequestProperty("Accept", "application/json");
+			JSONObject json = new JSONObject();
+			json.put("userId", member.getID());
+			json.put("setAdmin", true);
+			json.put("token", member.getToken());
+			String requestBody = json.toString();
 
-		JSONObject json = new JSONObject();
-		json.put("userId", member.getID());
-		json.put("setAdmin", true);
-		json.put("token", member.getToken());
-		String requestBody = json.toString();
+			OutputStreamWriter out = new OutputStreamWriter(conn.getOutputStream(), "UTF-8");
 
-		OutputStreamWriter out = new OutputStreamWriter(conn.getOutputStream(), "UTF-8");
+			out.write(requestBody);
+			out.close();
 
-		out.write(requestBody);
-		out.close();
+			//getResponseCode()!
 
-		//getResponseCode()!
+			BufferedReader in = new BufferedReader(
+													new InputStreamReader(
+															conn.getInputStream()));
 
-		BufferedReader in = new BufferedReader(
-												new InputStreamReader(
-														conn.getInputStream()));
+			StringBuffer result = new StringBuffer();
+			String line = "";
+			System.out.println("Output from Server .... \n");
+			while ((line = in.readLine()) != null) {
+				System.out.println(result);
+				result.append(line);
+			}
 
-		StringBuffer result = new StringBuffer();
-		String line = "";
-		System.out.println("Output from Server .... \n");
-		while ((line = in.readLine()) != null) {
-			System.out.println(result);
-			result.append(line);
-		}
+				//JSON string returned by server
+			JSONObject data = new JSONObject(result);
+			Bool success = data.getBoolean("success");
 
-			//JSON string returned by server
-		JSONObject data = new JSONObject(result);
-		Bool success = data.getBoolean("success");
+			if (success == true)
+				//admin was given
+				return true;
+			else {
+				//admin was not given
+				String message = data.getString("message");
+			}
+		        } catch (MalformedURLException e) {
 
-		if (success == true)
-			//setAdmin was successful!!
-		else {
-			//setAdmin failed
-			String message = data.getString("message");
-		}
+                e.printStackTrace();
 
-		return true;
+            } catch (IOException e) {
+
+                e.printStackTrace();
+            } catch (JSONException e) {
+
+                e.printStackTrace();
+     	}
+			
+			return false;
 	}
 
 	public bool revokeAdminPrivileges(household_admin admin)
@@ -93,6 +105,64 @@ public class household_admin extends household_member_class
 		//only allowed if role of caller = admin
 		admin.role = "member";
 
-		return true;
+				try {
+			String url = request + "setadmin";
+			//Get /household /roommates
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+			conn.setDoOutput(true);
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty("Content-Type", "application/json");
+			conn.setRequestProperty("Accept", "application/json");
+
+			JSONObject json = new JSONObject();
+			json.put("userId", member.getID());
+			json.put("setAdmin", false);
+			json.put("token", member.getToken());
+			String requestBody = json.toString();
+
+			OutputStreamWriter out = new OutputStreamWriter(conn.getOutputStream(), "UTF-8");
+
+			out.write(requestBody);
+			out.close();
+
+			//getResponseCode()!
+
+			BufferedReader in = new BufferedReader(
+													new InputStreamReader(
+															conn.getInputStream()));
+
+			StringBuffer result = new StringBuffer();
+			String line = "";
+			System.out.println("Output from Server .... \n");
+			while ((line = in.readLine()) != null) {
+				System.out.println(result);
+				result.append(line);
+			}
+
+				//JSON string returned by server
+			JSONObject data = new JSONObject(result);
+			Bool success = data.getBoolean("success");
+
+			if (success == true)
+				//admin was taken away
+				return true;
+			else {
+				//admin was not taken away
+				String message = data.getString("message");
+			}
+		        } catch (MalformedURLException e) {
+
+                e.printStackTrace();
+
+            } catch (IOException e) {
+
+                e.printStackTrace();
+            } catch (JSONException e) {
+
+                e.printStackTrace();
+     	}
+			
+			return false;
 	}
 }
