15ed2145175dc2f4de7bf138fd80118f494df3d1
Porowns <porowns@gmail.com>
2016-11-20 16:09:56 -0500

    Wrote several tests in create.js

--- server/test/households/create.js
+++ server/test/households/create.js
@@ -6,36 +6,120 @@ var chai = require('chai');
 var chaiHttp = require('chai-http');
 var should = chai.should();
 chai.use(chaiHttp);
+var utilities = require('./../utilities.js');
 
 var User = require('./../../models/user.js');
 var Household = require('./../../models/household.js');
 var createhousehold = require('./../../households/createhousehold.js');
 
 
-describe('createHouseholdTests', function() {
+describe('householdSuccessTests', function() {
+  var token;
   before(function(done) {
     utilities.makeCleanAccount({
       email: 'test@test',
       password: 'test123',
       username: 'test_username'
-    }, done);
+    }, function(err, res) {
+      utilities.loginToAccount({
+        email: 'test@test',
+        password: 'test123'
+      }, function(err, res) {
+        token = res.res.body.token;
+        done();
+      });
+    });
+  });
+  after(function(done) {
+    utilities.deleteHousehold({
+      token: token
+    }, function(err, res) {
+      utilities.deleteAccount({
+        email: 'test@test',
+        password: 'test123'
+      }, function(err, res) {
+        done();
+      });
+    });
+  });
+
+  describe('Create a Household', function() {
+    it('Creates a household', function(done) {
+      utilities.createHousehold({
+        houseHoldName: 'testhousehold',
+        houseHoldPassword: 'testpassword',
+        token: token
+      }, function(err, res) {
+        res.should.have.status(200);
+        res.res.body.should.have.property('success');
+        res.res.body.success.should.be.eql(true);
+        done();
+      });
+    });
+  });
+
+});
+
+describe ('householdFailureTests', function () {
+  var token;
+  before(function(done) {
+    utilities.makeCleanAccount({
+      email: 'test@test',
+      password: 'test123',
+      username: 'test_username'
+    }, function(err, res) {
+      utilities.loginToAccount({
+        email: 'test@test',
+        password: 'test123'
+      }, function(err, res) {
+        token = res.res.body.token;
+        done();
+      });
+    });
+  });
   after(function(done) {
     utilities.deleteAccount({
       email: 'test@test',
-      password: 'test123'
+      password: 'test123',
     }, done);
   });
 
-    utilities.loginToAccount({
-      email: 'test@test',
-      password: 'test123'
-    }, function(err, res) {
-      res.should.have.status(200);
-      res.res.body.should.have.property('success');
-      res.res.body.success.should.be.eql(true);
-      res.res.body.should.have.property('token');
-      res.res.body.token.should.have.length.of.at.least(1);
-      done();
+  describe('Create a household, no token, fail', function() {
+    it('Creates a household with no token', function(done) {
+      utilities.createHousehold({
+        houseHoldName: 'testhousehold',
+        houseHoldPassword: 'testpassword'
+      }, function(err, res) {
+        res.res.body.should.have.property('success');
+        res.res.body.success.should.be.eql(false);
+        done();
+      });
+    });
+  });
+
+  describe('Create a household, no name, fail', function() {
+    it('Creates a household with no name', function(done) {
+      utilities.createHousehold({
+        houseHoldPassword: 'testpassword',
+        token: token
+      }, function(err,res) {
+        res.res.body.should.have.property('success');
+        res.res.body.success.should.be.eql(false);
+        done();
+      });
+    });
+  });
+
+  describe('Create a household, no password, fail', function() {
+    it('Create a household with no password', function(done) {
+      utilities.createHousehold({
+        houseHoldName: 'testname',
+        token: token
+      }, function(err,res) {
+        res.res.body.should.have.property('success');
+        res.res.body.success.should.be.eql(false);
+        done();
+      });
     });
   });
 });
