39d31a483fefbde443f94223696cbd4727ed7d05
Simon Aizpurua <saa13b@my.fsu.edu>
2016-11-20 16:48:27 -0500

    Added a failure case

--- server/test/households/delete.js
+++ server/test/households/delete.js
@@ -8,7 +8,7 @@ chai.use(chaiHttp);
 
 var utilities = require('./../utilities.js');
 
-describe('deleteHouseholdTest', function() {
+describe('deleteHouseholdSuccessTest', function() {
   var token;
   before(function(done) {
       utilities.makeCleanAccount({
@@ -21,7 +21,7 @@ describe('deleteHouseholdTest', function() {
           password: 'test123'
         },  function(err, res) {
           token = res.res.body.token,
-          utilities.createHousehold({
+          utilities.createHouseHold({
             token: token,
             houseHoldName: 'houseHoldTest',
             houseHoldPassword: 'house123',
@@ -41,7 +41,57 @@ describe('deleteHouseholdTest', function() {
     it('delete the existing household', function(done) {
       utilities.deleteHousehold({
         token: token
-      }, done);
+      }, function(err, res) {
+        res.should.have.status(200);
+        res.res.body.should.have.property('success');
+        res.res.body.success.should.be.eql(true);
+        done();
+      });
     });
   });
+  // describe('')
+
+});
+
+describe('deleteHouseholdFailureTest', function() {
+  var token;
+  before(function(done) {
+      utilities.makeCleanAccount({
+        email: 'test@test',
+        password: 'test123',
+        username: 'test_username'
+      }, function(err, res) {
+        utilities.loginToAccount({
+          email: 'test@test',
+          password: 'test123'
+        },  function(err, res) {
+          token = res.res.body.token,
+          utilities.createHouseHold({
+            token: token,
+            houseHoldName: 'houseHoldTest',
+            houseHoldPassword: 'house123',
+          }, function(err, res) {
+            done();
+          });
+        });
+      });
+  });
+  after(function(done) {
+      utilities.deleteAccount({
+        email: 'test@test',
+        password: 'test123'
+      }, done);
+  });
+  describe('Delete the Household no token provided', function() {
+    it('delete the existing household w/ no token provided', function(done) {
+      utilities.deleteHousehold({
+        
+      }, function(err, res) {
+        res.res.body.should.have.property('success');
+        res.res.body.success.should.be.eql(false);
+        done();
+      });
+    });
+  });
+
 });
