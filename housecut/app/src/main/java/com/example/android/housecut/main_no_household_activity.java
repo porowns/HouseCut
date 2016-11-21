77496b410c026663695908db9527cfc94c88f490
JoseFernandes505 <jgf15e@my.fsu.edu>
2016-11-21 09:33:51 -0500

    Added Menu Bar to Main
    Added the menu bar to the main page with no household, along with minor bug fixes

--- housecut/app/src/main/java/com/example/android/housecut/main_no_household_activity.java
+++ housecut/app/src/main/java/com/example/android/housecut/main_no_household_activity.java
@@ -1,7 +1,11 @@
 package com.example.android.housecut;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 /**
  * Created by Jose on 11/21/2016.
@@ -12,6 +16,27 @@ public class main_no_household_activity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_confirmation);
+        Toolbar optionsToolbar = (Toolbar) findViewById(R.id.options_toolbar);
+        setSupportActionBar(optionsToolbar);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.options_toolbar, menu);
+        return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.settings_icon:
+                Intent intent = new Intent(main_no_household_activity.this, settings_activity.class);
+                startActivity(intent);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
 }
