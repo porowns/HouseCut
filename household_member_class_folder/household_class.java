6b14bd16eb0d84bb99814efaeb1c579ea498b53c
kpmcmanus <kpmcmanus@comcast.net>
2016-11-30 16:54:12 -0500

    added write to server function

--- household_member_class_folder/household_class.java
+++ household_member_class_folder/household_class.java
@@ -5,6 +5,17 @@ package com.housecut.housecut.household_member_class_folder;
 
 import java.util.Vector;
 import java.lang.String;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URLEncoder;
+import org.json.JSONObject;
+import org.json.JSONException;
 
 // Household class
 
@@ -13,12 +24,82 @@ public class household_class {
 	String householdName;
 	String currentAdmin;
 
+	// write to server
+	private JSONObject writeToServer (JSONObject json, String url) 
+	{
+
+          JSONObject data = new JSONObject();
+
+          try {
+            //Open a connection (to the server) for POST
+
+            URL url = new URL(url);
+
+            //Declare connection object
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            conn.setDoOutput(true);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty("Accept", "application/json");
+
+            String requestBody = json.toString();
+
+            //Opens up an outputstreamwriter for writing to server
+            //retrieve output stream that matches with Server input stream..
+            OutputStreamWriter out = new OutputStreamWriter(conn.getOutputStream(), "UTF-8");
+
+            //Write data to Server..
+            out.write(requestBody);
+            out.close();
+
+            /* If Response code isn't 200, throw exception. */
+
+            if (conn.getResponseCode() != 200) {
+              throw new IOException(conn.getResponseMessage());
+            }
+
+            //To test what the server outputs AND finish sending request
+            BufferedReader in = new BufferedReader(
+                              new InputStreamReader(
+                              conn.getInputStream()));
+
+            //StringBuffer will hold JSON string
+            StringBuffer result = new StringBuffer();
+            String line = "";
+            System.out.println("Output from Server .... \n");
+            while ((line = in.readLine()) != null) {
+               System.out.println(result);
+               result.append(line);
+            }
+
+            //JSON string returned by server
+            data = new JSONObject(result.toString());
+
+            //Closes everything
+            in.close();
+            conn.disconnect();
+
+
+            } catch (MalformedURLException e) {
+              e.printStackTrace();
+            } catch (IOException e) {
+              e.printStackTrace();
+            } catch (JSONException e) {
+              e.printStackTrace();
+            }
+            //Return JSON from server
+            return data;
+          }
+
+	
 	// household class constructor
 	public household_class(household_member_class admin, String name) {
 		// add the first member (the admin) to the household when the household is created
 		household_vector.add(admin);
 		currentAdmin = household_member_class.name;
 		householdName = name;
+		
 		
 	}
 	
